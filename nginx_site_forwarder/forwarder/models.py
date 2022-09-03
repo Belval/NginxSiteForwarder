a51c4c4705093de955f0de08da6419fb3eff2dbf
Edouard Belval <edouard@belval.org>
2022-09-03 16:57:28 -0400

    Restart nginx

--- nginx_site_forwarder/forwarder/models.py
+++ nginx_site_forwarder/forwarder/models.py
@@ -1,10 +1,9 @@
-from os import remove
 from django.contrib import admin
 from django.db import models
 from django.db.models.signals import pre_save, pre_delete
 from django.dispatch import receiver
 
-from forwarder.utils import add_config, remove_config, run_certbot
+from forwarder.utils import add_config, remove_config, run_certbot, restart_nginx
 
 class Forwarding(models.Model):
     subdomain = models.CharField(max_length=40)
@@ -19,7 +18,10 @@ class ForwardingAdmin(admin.ModelAdmin):
 def create_or_save(sender, instance, raw, using, update_fields, **kwargs):
     add_config(instance)
     run_certbot()
+    restart_nginx()
+
 
 @receiver(pre_delete, sender=Forwarding)
 def delete(sender, instance, **kwargs):
     remove_config(instance)
+    restart_nginx()
